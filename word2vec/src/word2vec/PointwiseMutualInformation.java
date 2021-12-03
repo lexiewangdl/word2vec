02351a28a928e858ac94604b5947fa6514f70dd3
lexiewangdl <travelaroundthemoon@gmail.com>
2021-12-03 23:39:44 +0800

    update coocurrence matrix with pmi values

--- word2vec/src/word2vec/PointwiseMutualInformation.java
+++ word2vec/src/word2vec/PointwiseMutualInformation.java
@@ -1,6 +1,8 @@
 package word2vec;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 
 public class PointwiseMutualInformation {
 
@@ -57,6 +59,21 @@ public class PointwiseMutualInformation {
 		double valuePMI = Math.log(jointPct / (pt * pc));
 		return valuePMI;
 	}
+	
+	public double[][] updateCoocurrence(double[][] coocurrence){
+		double[][] newCoocurrence = new double[coocurrence[0].length][coocurrence[0].length];
+		for (int t=0; t<coocurrence[0].length; t++) {
+			for (int c=0; c<coocurrence[0].length; c++) {
+				double PMI = calculatePMI(coocurrence, c, t);
+				if (PMI<=0) {
+					newCoocurrence[c][t] = 0.0;
+				} else {
+					newCoocurrence[c][t] = calculatePMI(coocurrence, c, t);
+				}
+			}
+		}
+		return newCoocurrence;
+	}
 
 	public static void main(String[] args) {
 		SparseVector sv = new SparseVector();
@@ -89,7 +106,22 @@ public class PointwiseMutualInformation {
 		// target word: dog, index = 2
 		double PMIValue = pmi.calculatePMI(testMatrix, 7, 2);
 		System.out.println("Value of PMI of context word chased about target word dog is " + PMIValue);
-
+		
+		double[][] newMatrix = pmi.updateCoocurrence(testMatrix);
+		System.out.println(Arrays.deepToString(newMatrix));
+		
+		/*
+		 * [[0.0, 0.0, 0.4149438520627083, 0.0, 0.0, 0.6380874033769179, 0.48393672354965966, 0.0, 0.6380874033769179, 0.6380874033769179],
+		 *  [0.0, 0.0, 0.42933258951480785, 0.14165051706302664, 0.0, 0.0, 0.4983254610017593, 0.834797697622972, 0.14165051706302664, 0.5471156251711912], 
+		 *  [0.4149438520627083, 0.42933258951480785, 0.0, 0.5690945318899665, 0.0, 0.0, 0.009478743954543739, 0.5690945318899665, 0.0, 0.0], 
+		 *  [0.0, 0.14165051706302664, 0.5690945318899665, 0.0, 1.1977031913123406, 1.1977031913123406, 0.0, 0.0, 0.0, 0.5045560107523953], 
+		 *  [0.0, 0.0, 0.0, 1.1977031913123406, 0.0, 1.1977031913123406, 0.6380874033769179, 0.0, 0.0, 0.5045560107523953],
+		 *  [0.6380874033769179, 0.0, 0.0, 1.1977031913123406, 1.1977031913123406, 0.0, 0.6380874033769179, 0.0, 0.0, 0.0],
+		 *  [0.48393672354965966, 0.4983254610017593, 0.009478743954543739, 0.0, 0.6380874033769179, 0.6380874033769179, 0.0, 0.0, 0.6380874033769179, 0.0],
+		 *  [0.0, 0.834797697622972, 0.5690945318899665, 0.0, 0.0, 0.0, 0.0, 0.0, 1.1977031913123406, 0.5045560107523953],
+		 *  [0.6380874033769179, 0.14165051706302664, 0.0, 0.0, 0.0, 0.0, 0.6380874033769179, 1.1977031913123406, 0.0, 0.0],
+		 *  [0.6380874033769179, 0.5471156251711912, 0.0, 0.5045560107523953, 0.5045560107523953, 0.0, 0.0, 0.5045560107523953, 0.0, 0.0]]
+		 */
 	}
 
 }
